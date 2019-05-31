a0ef380c38394cdd6e7cfc2ebe4daff322775622
alan <alan606@gmail.com>
2019-05-31 12:43:23 -0400

    testing preload

--- frontEnd/src/conponents/WelcomePageSlide/WelcomePageSlide.js
+++ frontEnd/src/conponents/WelcomePageSlide/WelcomePageSlide.js
@@ -4,8 +4,7 @@ import slide1 from '../../photo/slide1.jpg';
 import slide2 from '../../photo/slide2.jpg';
 import slide3 from '../../photo/slide3.jpg';
 import './WelcomePageSlide.css'
-
-
+import {Spinner} from 'reactstrap'
 
 
 
@@ -36,16 +35,29 @@ class Slide extends Component {
               }
             ],
             slide1Ready: false,
+            slide2Ready: false,
+            slide3Ready: false
           }
         }
 
       componentDidMount(){
         var img1 = new Image();
-        img1.src = slide3;
+        img1.src = slide1;
         img1.onload=()=>
         {
            this.setState({slide1Ready: true})
         }
+        var img2 = new Image();
+        img2.src = slide2;
+        img2.onload=()=>{
+            this.setState({slide2Ready: true})
+        }
+        var img3 = new Image();
+        img3.src = slide3;
+        img3.onload=()=>{
+            this.setState({slide3Ready: true})
+        }
+
       }
 
     
@@ -55,7 +67,7 @@ class Slide extends Component {
 
 
 render(){
-    if(this.state.slide1Ready)
+    if(this.state.slide1Ready&&this.state.slide2Ready&&this.state.slide3Ready)
     {
     return(   
       <UncontrolledCarousel className="wps_" items={this.state.items} />
@@ -64,7 +76,8 @@ render(){
     else
     {
       return(
-        <p></p>
+
+        <Spinner color="primary" style={{ width: '3rem', height: '3rem' }}/>
       )
     }
 }
