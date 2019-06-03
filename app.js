110804470024a7f06f7fba780b3362899cc436ff
alan <alan606@gmail.com>
2019-06-03 16:20:06 -0400

    dfff

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const app = express();
 const port = process.env.PORT||5000 ;
 
 app.use(express.static(path.join(__dirname, 'frontEnd/build')));
+//refresh
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontEnd/build/index.html'));
+  });
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
